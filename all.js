da79efb017cf479123144ceaf5ed8c435e36dae7
han0005 <v9958v@yahoo.com.tw>
2024-02-05 17:17:52 +0800

    0205

--- all.js
+++ all.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    /*
     $('.footer-down li').click(function (e) { 
         e.preventDefault();
         $(this).find('.footer-links').toggleClass('open').parent().siblings().find('.footer-links').removeClass('open');
@@ -9,7 +10,7 @@ $(document).ready(function () {
         e.preventDefault();
         $(this).find('h2').toggleClass('hv').parent().siblings().find('h2').removeClass('hv');
     });
-
+     */
     $('.aside-btn').click(function (e) { 
         e.preventDefault();
         $('.aside').toggleClass('aside-active');
